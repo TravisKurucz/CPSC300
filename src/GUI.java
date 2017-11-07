e0323827d72e7e3427f32688b1084e3de99f0a75
Unknown <coltonaarts@gmail.com>
2017-11-07 14:05:52 -0800

    More GUI Changes
    stuff

--- src/GUI.java
+++ src/GUI.java
@@ -1,17 +1,19 @@
+import PartOrders.PartOrder;
+import UpdateTables.UpdatePartOrder;
 import javafx.application.Application;
-import javafx.beans.property.ReadOnlyStringWrapper;
-import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-import java.util.ArrayList;
 
 public class GUI extends Application {
 
@@ -23,18 +25,11 @@ public class GUI extends Application {
     public void start(Stage primaryStage)
     {
         primaryStage.setTitle("Login");
+
         GridPane grid = new GridPane();
+
         Button btn = new Button();
         btn.setText("Login");
-        TextArea login = new TextArea();
-        TextArea pass = new TextArea();
-        Text loginWord = new Text();
-        Text passWord = new Text();
-        loginWord.setText("User Name:");
-        passWord.setText("Password");
-        grid.setHgap(10);
-        grid.setVgap(10);
-        grid.setPadding(new Insets(25,25,25,25));
         btn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
@@ -42,80 +37,120 @@ public class GUI extends Application {
                 primaryStage.close();
             }
         });
+
+        TextArea login = new TextArea();
+
+        TextArea pass = new TextArea();
+
+        Text loginWord = new Text();
+        loginWord.setText("User Name:");
+
+        Text passWord = new Text();
+        passWord.setText("Password:");
+
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25,25,25,25));
         grid.setAlignment(Pos.CENTER);
         grid.add(loginWord, 0,0,2,1);
         grid.add(login, 0,1,2,1);
         grid.add(passWord,0,2,2,1);
         grid.add(pass, 0,3,2,1);
         grid.add(btn, 0,4,2,1);
+
         Scene scene = new Scene(grid, 300, 275);
+
         primaryStage.setScene(scene);
         primaryStage.show();
-
-
-
     }
 
     private void MainGUI()
     {
         Stage primaryStage = new Stage();
         primaryStage.setTitle("Inventory Management");
+
         Button btn = new Button();
-        Button btn2 = new Button();
-        Button btn3 = new Button();
-        Button btn4 = new Button();
         btn.setText("Part Order");
-        btn2.setText("Work Order");
-        btn3.setText("Report");
-        btn4.setText("Test");
+        btn.setMaxWidth(Double.MAX_VALUE);
         btn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 PartOrder();
             }
         });
+
+        Button btn2 = new Button();
+        btn2.setText("Work Order");
+        btn2.setMaxWidth(Double.MAX_VALUE);
         btn2.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 WorkOrder();
             }
         });
+
+        Button btn3 = new Button();
+        btn3.setText("Report");
+        btn3.setMaxWidth(Double.MAX_VALUE);
         btn3.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 Report();
             }
         });
+
+        Button btn4 = new Button();
+        btn4.setText("Test");
+        btn4.setMaxWidth(Double.MAX_VALUE);
         btn4.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 Test();
             }
         });
+
         Text text = new Text();
         text.setText("Error Area");
+
         TextArea area = new TextArea();
+        area.setEditable(false);
+
+        VBox vbButtons = new VBox();
+        vbButtons.setSpacing(10);
+        vbButtons.setPadding(new Insets(0, 20, 10, 20));
+        vbButtons.getChildren().addAll(btn, btn2, btn3, btn4);
+
+
         GridPane grid = new GridPane();
         grid.setAlignment(Pos.TOP_LEFT);
         grid.setHgap(10);
         grid.setVgap(10);
         grid.setPadding(new Insets(25,25,25,25));
-        grid.add(btn, 0, 0, 2, 1);
-        grid.add(btn2, 0,1,2,1);
-        grid.add(btn3,0,2,2,1);
-        grid.add(text,3,2,2,1);
-        grid.add(area, 3,3,2,1);
-        grid.add(btn4, 0,3, 2, 1);
-        Scene scene = new Scene(grid, 300, 275);
+        grid.add(vbButtons, 0,0,2,1);
+        grid.add(text,2,2,2,1);
+        grid.add(area, 2,3,2,1);
+        Scene scene = new Scene(grid, 600, 600);
+
         primaryStage.setScene(scene);
+        primaryStage.setMaximized(true);
         primaryStage.show();
     }
 
     private void PartOrder()
     {
         Stage stage = new Stage();
-
         stage.setTitle("Part Order");
+
+        BorderPane borderPending = new BorderPane();
+        HBox buttons = new HBox();
+
+        Button one = new Button("One");
+        Button two = new Button("Two");
+
+        buttons.getChildren().addAll(one, two);
+        buttons.setSpacing(10);
+        buttons.setAlignment(Pos.BOTTOM_CENTER);
+        buttons.setMinHeight(40);
 
         Tab pending = new Tab("Pending");
         pending.setClosable(false);
@@ -123,8 +158,13 @@ public class GUI extends Application {
         TableColumn numberPending = new TableColumn("Number");
         TableColumn supplierPending = new TableColumn("Supplier");
         TableColumn orderedByPending = new TableColumn("Ordered By");
+        UpdatePartOrder.updatePending(numberPending,supplierPending,orderedByPending);
         tableViewPending.getColumns().addAll(numberPending, supplierPending, orderedByPending);
-        pending.setContent(tableViewPending);
+
+
+        borderPending.setCenter(tableViewPending);
+        borderPending.setBottom(buttons);
+        pending.setContent(borderPending);
 
         Tab outstanding = new Tab("Outstanding");
         outstanding.setClosable(false);
@@ -144,18 +184,10 @@ public class GUI extends Application {
         tableViewFinalized.getColumns().addAll(numberFinalized, supplierFinalized, orderedByFinalized);
         finalized.setContent(tableViewFinalized);
 
-
-
-
         TabPane tabPane = new TabPane(pending, outstanding, finalized);
-        GridPane grid = new GridPane();
-        grid.setAlignment(Pos.TOP_LEFT);
-        grid.setHgap(10);
-        grid.setVgap(10);
-        grid.setPadding(new Insets(25,25,25,25));
-        grid.add(tabPane, 1,1);
-        Scene scene = new Scene(grid, 500, 600);
+        Scene scene = new Scene(tabPane, 700, 800);
         stage.setScene(scene);
+        stage.setMaximized(true);
         stage.show();
     }
 
@@ -163,6 +195,7 @@ public class GUI extends Application {
     {
         Stage stage = new Stage();
         stage.setTitle("Work Order");
+        stage.setMaximized(true);
         stage.show();
     }
 
@@ -170,6 +203,7 @@ public class GUI extends Application {
     {
         Stage stage = new Stage();
         stage.setTitle("Report");
+        stage.setMaximized(true);
         stage.show();
     }
 
---
+++ src/PartOrders/PartOrder.java
@@ -0,0 +1,50 @@
+package PartOrders;
+
+
+import javafx.beans.property.SimpleStringProperty;
+
+/**
+ * Created by colto on 2017-11-07.
+ */
+public class PartOrder
+{
+    private int number;
+    private SimpleStringProperty supplier;
+    private SimpleStringProperty orderBy;
+
+    public PartOrder(int number, String supplier, String orderBy)
+    {
+        this.number = number;
+        this.supplier = new SimpleStringProperty(supplier);
+        this.orderBy = new SimpleStringProperty(orderBy);
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getSupplier() {
+        return supplier.get();
+    }
+
+    public void setSupplier(String supplier) {
+        this.supplier.set(supplier);
+    }
+
+    public String getOrderBy() {
+        return orderBy.get();
+    }
+
+    public void setOrderBy(String orderBy) {
+        this.orderBy.set(orderBy);
+    }
+
+
+
+
+
+}
